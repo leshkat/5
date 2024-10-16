3ca644ceaf004a77dee96143303af063b5335ea2
Alona <126986191+leshkat@users.noreply.github.com>
2024-10-16 16:14:23 +0300

    Add files via upload

---
+++ part1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+public class Piesa : IDisposable
+{
+    public string Nazva { get; set; }
+    public string Avtor { get; set; }
+    public string Zhanr { get; set; }
+    public int Rik { get; set; }
+    private bool disposed = false;
+
+    public Piesa(string nazva, string avtor, string zhanr, int rik)
+    {
+        Nazva = nazva;
+        Avtor = avtor;
+        Zhanr = zhanr;
+        Rik = rik;
+        Console.WriteLine("П'єса створена.");
+    }
+
+    ~Piesa()
+    {
+        Dispose(false);
+        Console.WriteLine("Деструктор викликаний.");
+    }
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"Назва: {Nazva}, Автор: {Avtor}, Жанр: {Zhanr}, Рік: {Rik}");
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposed)
+        {
+            if (disposing)
+            {
+                Console.WriteLine("Звільнення керованих ресурсів.");
+            }
+            disposed = true;
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        CultureInfo.CurrentCulture = new CultureInfo("uk-UA");
+        CultureInfo.CurrentUICulture = new CultureInfo("uk-UA");
+
+        Console.WriteLine("Тестування класу «П'єса»:");
+        using (var piesa = new Piesa("Гамлет", "Вільям Шекспір", "Трагедія", 1600))
+        {
+            piesa.ShowInfo();
+        }
+        Console.WriteLine("Кінець програми.");
+    }
+}
---
+++ part2.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+public enum TypMagazynu
+{
+    Prodyktovyi,
+    Hospodarskyi,
+    Odiag,
+    Vzuttia
+}
+
+public class Magazin : IDisposable
+{
+    public string Nazva { get; set; }
+    public string Adresa { get; set; }
+    public TypMagazynu Typ { get; set; }
+    private bool disposed = false;
+
+    public Magazin(string nazva, string adresa, TypMagazynu typ)
+    {
+        Nazva = nazva;
+        Adresa = adresa;
+        Typ = typ;
+        Console.WriteLine("Магазин створений.");
+    }
+
+    ~Magazin()
+    {
+        Dispose(false);
+        Console.WriteLine("Деструктор магазину викликаний.");
+    }
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"Назва: {Nazva}, Адреса: {Adresa}, Тип: {Typ}");
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposed)
+        {
+            if (disposing)
+            {
+                Console.WriteLine("Звільнення керованих ресурсів магазину.");
+            }
+            disposed = true;
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        CultureInfo.CurrentCulture = new CultureInfo("uk-UA");
+        CultureInfo.CurrentUICulture = new CultureInfo("uk-UA");
+
+        Console.WriteLine("Тестування класу «Магазин»:");
+        using (var magazin = new Magazin("АТБ", "вул. Соборна, 12", TypMagazynu.Prodyktovyi))
+        {
+            magazin.ShowInfo();
+        }
+        Console.WriteLine("Кінець програми.");
+    }
+}
